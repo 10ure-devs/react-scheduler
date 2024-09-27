493d06f6686d4e0da460787e099b0bb41d988f7f
programminPete <peter.fasula@gmail.com>
2024-09-27 15:34:09 -0400

    attempting to fix scroll

--- src/components/Calendar/styles.ts
+++ src/components/Calendar/styles.ts
@@ -4,6 +4,7 @@ import { leftColumnWidth } from "@/constants";
 export const StyledOuterWrapper = styled.div`
   position: relative;
   display: flex;
+  min-height: 100%;
 `;
 
 export const StyledInnerWrapper = styled.div`
@@ -12,5 +13,5 @@ export const StyledInnerWrapper = styled.div`
   display: flex;
   flex-direction: column;
   contain: paint;
-  min-height: 500px;
+  min-height: 100%;
 `;
